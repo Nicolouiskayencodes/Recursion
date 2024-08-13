1990a5c53755ecfb9240dc4efc818c3d8402f6f1
Nicolas Louis-Kayen <nicolouiskayencodes@gmail.com>
2024-08-12 23:12:37 -0700

    Create merge sort function

--- script.js
+++ script.js
@@ -17,16 +17,53 @@ function fibs(num) {
 
 function fibsRec(num) {
   let fibArray = [];
-  if (num<0){
+  if (num===0){
     return fibArray;
   }
   function fib(n) {
-    if (n <2) {
-      return n;
+    if (n===1){
+      return 0;
+    } else if (n === 2) {
+      return 1;
     } else {
       return (fib(n-1) + fib(n-2))
     };
   }
   fibArray.push(fib(num));
   return fibsRec(num-1).concat(fibArray);
+}
+
+function mergeSort(array) {
+  if (array.length <= 1) {
+    console.log(array)
+    return array;
+  } else {
+    let left = array.slice(0,Math.round(array.length/2));
+    let right = array.slice(Math.round(array.length/2), (array.length));
+    let sortedLeft = mergeSort(left);
+    let sortedRight = mergeSort(right);
+    function merge(leftSide, rightSide){
+      let newArray = [];
+      let i = 0;
+      let j = 0;
+      let k = 0;
+      while (i<leftSide.length && j<rightSide.length){
+        if (leftSide[i]<rightSide[j]){
+          newArray[k++] = leftSide[i++]
+        } else if (leftSide[i]>=rightSide[j]){
+          newArray[k++] = rightSide[j++]
+        }
+      }
+      for (; i<leftSide.length; i++) {
+        newArray[k++] = leftSide[i]
+      }
+      for (; j<rightSide.length; j++) {
+        newArray[k++] = rightSide[j]
+      }
+      console.log(newArray)
+      return newArray;
+    }
+    return merge(sortedLeft, sortedRight);
+  }
+  
 }
